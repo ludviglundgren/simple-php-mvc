43ffcc6d303d04ad39d4fcc8a0d89e92e57add94
Ludvig Lundgren <hello@ludviglundgren.se>
2017-10-29 20:33:48 +0100

    Add dynamic namespace to router and controllers

--- Core/Router.php
+++ Core/Router.php
@@ -60,7 +60,7 @@ class Router
         if ($this->match($url)) {
             $controller = $this->params['controller'];
             $controller = $this->convertToStudlyCaps($controller);
-            $controller = "App\Controllers\\$controller";
+            $controller = $this->getNamespace() . $controller;
 
             if (class_exists($controller)) {
                 $controller_object = new $controller($this->params);
@@ -104,6 +104,17 @@ class Router
         }
 
         return $url;
+    }
+
+    protected function getNamespace()
+    {
+        $namespace = 'App\Controllers\\';
+
+        if (array_key_exists('namespace', $this->params)) {
+            $namespace .= $this->params['namespace'] . '\\';
+        }
+
+        return $namespace;
     }
 
 }
