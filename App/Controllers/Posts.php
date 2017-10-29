575c2dc1fb4a792bd2373fc6811c9ef9426b7e90
Ludvig Lundgren <hello@ludviglundgren.se>
2017-10-29 18:54:54 +0100

    Added first controller for posts.php

---
+++ App/Controllers/Posts.php
@@ -0,0 +1,14 @@
+<?php
+
+class Posts
+{
+    public function index()
+    {
+        echo 'Hello from the index action in the Posts controller!';
+    }
+
+    public function addNew()
+    {
+        echo 'Hello from the addNew action in the Posts controller!';
+    }
+}
