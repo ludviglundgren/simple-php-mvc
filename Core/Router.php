92a351b78749dbadb6726ffe8cb6a0aec33cb992
Ludvig Lundgren <hello@ludviglundgren.se>
2017-10-29 18:27:40 +0100

    Initial commit. Router added.

---
+++ Core/Router.php
@@ -0,0 +1,53 @@
+<?php
+
+class Router
+{
+    protected $routes = [];
+    protected $params = [];
+
+    public function add($route, $params = [])
+    {
+        // Convert the route to a regular expression: escape forward slashes
+        $route = preg_replace('/\//', '\\/', $route);
+
+        // convert variables e.g {controller}
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+
+        // convert variables with custom reqexp e.g {id:\d+}
+        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+
+        // Add start and end delimiters, and case insensitive flag
+        $route = '/^' . $route . '$/i';
+
+        $this->routes[$route] = $params;
+    }
+
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
+
+    public function match($url)
+    {
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+    
+                foreach ($matches as $key => $match) {
+                    if (is_string($key)) {
+                        $params[$key] = $match;
+                    }
+                }
+    
+                $this->params = $params;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function getParams()
+    {
+        return $this->params;
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,28 @@
+<?php
+
+// echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
+
+require '../Core/Router.php';
+
+$router = new Router();
+
+// Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('{controller}/{action}');
+$router->add('{controller}/{id:\d+}/{action}');
+
+echo '<pre>';
+    // var_dump($router->getRoutes());
+echo htmlspecialchars(print_r($router->getRoutes(), true));
+echo '</pre>';
+
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+    echo '<pre>';
+        var_dump($router->getParams());
+    echo '</pre>';
+} else {
+    echo "Route not found for URL '$url'";
+}
